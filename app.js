587482ce9f93c0ce8bdafbf14947a9239143a27b
Ryan Manro <rmanro@gmail.com>
2018-01-31 14:52:33 -0800

    cleaned up code on all questions and added correct # of guesses

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 'use strict';
 
+let correctGuesses = 0; // tracking # of correct guesses
+
 // Asking for user's name
 const userName = prompt('What is your name?');
 console.log('User\'s Name = ' + userName);
 alert('Nice to meet you ' + userName + '! My name is Ryan. We will now play a game where I ask you five questions about myself. You can answer with \'yes\' or \'no\'. Good luck!');
 
 // Question 1
-const hometown = prompt('Was I born in Las Vegas?');
+const hometown = prompt('Was I born in Las Vegas?').toLowerCase();
 console.log(userName + '\'s answer to Question 1 was: ' + hometown);
-if (hometown.toLowerCase() === 'no' || hometown.toLowerCase() === 'n'){
+if (hometown === 'no' || hometown === 'n'){
     alert('That\'s right ' + userName + ', I was born in Orange, California!');
-} else if (hometown.toLowerCase() === 'yes' || hometown.toLowerCase() === 'y'){
+    correctGuesses = correctGuesses + 1;
+} else if (hometown === 'yes' || hometown === 'y'){
     alert('Sorry ' + userName + ', but I was born in Orange, California.');
 } else{
     alert('Sorry ' + userName + ', but I didn\'t understand that answer.');
 }
 
 // Question 2
-const favOlives = prompt('Do I like green olives?');
+const favOlives = prompt('Do I like green olives?').toLowerCase();
 console.log(userName + '\'s answer to Question 2 was: ' + favOlives);
-if (favOlives.toLowerCase() === 'no' || favOlives.toLowerCase() === 'n'){
+if (favOlives === 'no' || favOlives === 'n'){
     alert('That\'s right ' + userName + ', I only like black olives!');
-} else if (favOlives.toLowerCase() === 'yes' || favOlives.toLowerCase() === 'y'){
+    correctGuesses = correctGuesses + 1;
+} else if (favOlives === 'yes' || favOlives === 'y'){
     alert('Sorry ' + userName + ', but I only like black olives.');
 } else{
     alert('Sorry ' + userName + ', but I didn\'t understand that answer.');
@@ -29,11 +33,12 @@ if (favOlives.toLowerCase() === 'no' || favOlives.toLowerCase() === 'n'){
 
 // Question 3
 
-const tattoo = prompt('Do I have any tattoos?');
+const tattoo = prompt('Do I have any tattoos?').toLowerCase();
 console.log(userName + '\'s answer to Question 3 was: ' + tattoo);
-if (tattoo.toLowerCase() === 'yes' || tattoo.toLowerCase() === 'y'){
+if (tattoo === 'yes' || tattoo === 'y'){
     alert('That\'s right ' + userName + ', I have one tattoo on my arm!');
-} else if (tattoo.toLowerCase() === 'no' || tattoo.toLowerCase() === 'n'){
+    correctGuesses = correctGuesses + 1;
+} else if (tattoo === 'no' || tattoo === 'n'){
     alert('Sorry ' + userName + ', but I have one tattoo.');
 } else{
     alert('Sorry ' + userName + ', but I didn\'t understand that answer.');
@@ -41,11 +46,12 @@ if (tattoo.toLowerCase() === 'yes' || tattoo.toLowerCase() === 'y'){
 
 // Question 4
 
-const civ = prompt('Is Civilization my favorite computer game?');
+const civ = prompt('Is Civilization my favorite computer game?').toLowerCase();
 console.log(userName + '\'s answer to Question 4 was: ' + civ);
-if (civ.toLowerCase() === 'yes' || civ.toLowerCase() === 'y'){
+if (civ === 'yes' || civ === 'y'){
     alert('That\'s right ' + userName + ', I\'ve been playing it since I was a kid!');
-} else if (civ.toLowerCase() === 'no' || civ.toLowerCase() === 'n'){
+    correctGuesses = correctGuesses + 1;
+} else if (civ === 'no' || civ === 'n'){
     alert('Sorry ' + userName + ', but that is definitely my favorite computer game.');
 } else{
     alert('Sorry ' + userName + ', but I didn\'t understand that answer.');
@@ -53,15 +59,18 @@ if (civ.toLowerCase() === 'yes' || civ.toLowerCase() === 'y'){
 
 // Question 5
 
-const soccer = prompt('Is Soccer my favorite sport?');
+const soccer = prompt('Is Soccer my favorite sport?').toLowerCase();
 console.log(userName + '\'s answer to Question 5 was: ' + soccer);
-if (soccer.toLowerCase() === 'yes' || soccer.toLowerCase() === 'y'){
+if (soccer === 'yes' || soccer === 'y'){
     alert('That\'s right ' + userName + ', that is my favorite sport!');
-} else if (soccer.toLowerCase() === 'no' || soccer.toLowerCase() === 'n'){
+    correctGuesses = correctGuesses + 1;
+} else if (soccer === 'no' || soccer === 'n'){
     alert('Sorry ' + userName + ', but soccer is my favorite sport.');
 } else{
     alert('Sorry ' + userName + ', but I didn\'t understand that answer.');
 }
+
+alert('Now I\'m going to ask you a couple more questions about me. You will have a few tries on each. Good luck!');
 
 // Question 6 (four guesses)
 
@@ -69,6 +78,7 @@ for (let i = 0; i < 4; i++){
     const states = parseInt(prompt('How many states have I lived in?'));
     if (states === 3){
         alert('That\'s correct ' + userName + '! I\'ve lived in California, Nevada, and Oregon.');
+        correctGuesses = correctGuesses + 1;
         break;
     } else if (i === 3){
         alert('That\'s incorrect. You have no more guesses. Sorry!');
@@ -85,13 +95,19 @@ const techsupport = ['dell', 'adobe', 'shopkeep'];
 for (let i = 0; i < 6; i++){
     const techAns = prompt('Can you guess a company I\'ve done tech support for?').toLowerCase();
     if (techsupport.indexOf(techAns) === -1){
-        alert('That\'s incorrect. Please guess again.');
+        if (i < 5){
+            alert('That\'s incorrect. Please guess again.');
+        } else {
+            alert('Sorry, but you have no more guesses.');
+            break;
+        }
     } else {
         alert('That\'s correct ' + userName + '! I\'ve worked for Dell, Adobe, and ShopKeep.');
+        correctGuesses = correctGuesses + 1;
         break;
     }
 }
 
 // Goodbye message
 
-alert('Thanks for playing ' + userName + '! See ya later!');
+alert('Thanks for playing ' + userName + '! You got ' + correctGuesses + ' out of 7 questions right. See ya later!');
